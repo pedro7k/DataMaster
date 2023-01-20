29dd1182648b5888ad3fd59cfb13984d07c1db46
pedro <634115264@qq.com>
2023-01-20 14:36:05 +0800

    2023.1.20 fix单表扫描功能

--- DataMaster-application/src/main/java/com/pedro/application/process/impl/DataScanProcessImpl.java
+++ DataMaster-application/src/main/java/com/pedro/application/process/impl/DataScanProcessImpl.java
@@ -4,13 +4,19 @@ import com.pedro.application.process.DataScanProcess;
 import com.pedro.domain.dbProcess.model.res.TableScanRes;
 import com.pedro.domain.dbProcess.service.tableScan.TableScanService;
 import com.pedro.domain.score.service.TableHealthScoreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class DataScanProcessImpl implements DataScanProcess {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataScanProcessImpl.class);
 
     @Resource
     private TableScanService tableScanService;
@@ -25,12 +31,18 @@ public class DataScanProcessImpl implements DataScanProcess {
         List<TableScanRes> tableScanResList = tableScanService.scanTable(tid);
 
         // 2.单表打分
-        Double score = tableHealthScoreService.doTableScore(tableScanResList);
+        double score;
+        if (CollectionUtils.isEmpty(tableScanResList)) {
+            score = 100.0;
+        } else {
+            score = tableHealthScoreService.doTableScore(tableScanResList);
+        }
 
         // 3.打分结果落库
         tableHealthScoreService.insertTableHealthScore(tid, score);
 
         // 4.返回结果
+        logger.info("执行单表扫描完成，tid={}，score={}，time={}", tid, score, new Date());
         return score;
     }
 }
